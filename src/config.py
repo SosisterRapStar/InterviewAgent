56e1ad256db894cb21e7297a8c3195ea706b7098
SosisterRapStar <timoshenkovvanya@gmail.com>
2026-01-30 18:31:51 +0300

    deleted slop

--- src/config.py
+++ src/config.py
@@ -3,11 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv() 
 
-DEEPSEEK_TOKEN: str = os.getenv("DEEPSEEK_TOKEN")
-OPENROUTER_TOKEN: str = os.getenv("OPENROUTER_TOKEN")
 MISTRAL_TOKEN: str = os.getenv("MISTRAL_KEY")
-MINIO_ACCESS_KEY: str = os.getenv("MINIO_ACCESS_KEY")
-MINIO_SECRET_KEY: str = os.getenv("MINIO_SECRET_KEY")
-MINIO_ENDPOINT: str = os.getenv("MINIO_ENDPOINT")
-MINIO_BUCKET: str = os.getenv("MINIO_BUCKET")
 LOKI_ENDPOINT: str = os.getenv("LOKI_ENDPOINT")
--- src/utils.py
+++ src/utils.py
@@ -2,12 +2,9 @@ from langchain_mistralai import ChatMistralAI
 from src.config import MISTRAL_TOKEN
 
 
-# Mistral models
-# https://docs.mistral.ai/getting-started/models/models_overview/
-MISTRAL_MODEL = "mistral-large-latest"  # Или "open-mistral-7b", "mistral-medium-latest"
+MISTRAL_MODEL = "mistral-large-latest"
 
 def get_openrouter_llm(model=MISTRAL_MODEL):
-    """Возвращает LLM для использования в агентах (теперь Mistral)."""
     return ChatMistralAI(
         model=model,
         api_key=MISTRAL_TOKEN,
